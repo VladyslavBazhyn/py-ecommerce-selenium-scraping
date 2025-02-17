420a0462d73c704fb43c18616880392e39ab11b9
Gibbs <mr.darmstadtiumgmail.com>
2025-02-17 15:51:20 +0100

    Wrote functions to get one product name

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,26 @@
 from dataclasses import dataclass
 from urllib.parse import urljoin
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
+
+options = Options()
+
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+
 
 BASE_URL = "https://webscraper.io/"
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
+
+COMPUTERS_URL = urljoin(HOME_URL, "computers/")
+LAPTOPS_URL = urljoin(COMPUTERS_URL, "laptops/")
+TABLETS_URL = urljoin(COMPUTERS_URL, "tablets/")
+
+PHONES_URL = urljoin(HOME_URL, "phones/")
+TOUCH_PHONES_URL = urljoin(PHONES_URL, "touch/")
 
 
 @dataclass
@@ -15,9 +32,32 @@ class Product:
     num_of_reviews: int
 
 
-def get_all_products() -> None:
-    pass
+def get_single_product(product: WebElement) -> Product:
+    name = product.find_element(By.CLASS_NAME, "title").get_attribute("title")
+    print(name)
+
+
+def get_page_products(link: str) -> list[Product]:
+    with webdriver.Chrome(options=options) as driver:
+        driver.get(link)
+        web_product = driver.find_element(By.CLASS_NAME, "card")
+        product = get_single_product(web_product)
+        # print(type(product))
+
+
+def get_all_products(links: list) -> None:
+    for link in links:
+        file_name = f"{link.split('/')[-2]}.csv"
+
+        page_products = get_page_products(link)
+
+        with open(file_name, "w", encoding="utf-8") as f:
+            # print(file_name)
+            pass
 
 
 if __name__ == "__main__":
-    get_all_products()
+    all_links = [
+        HOME_URL, COMPUTERS_URL, LAPTOPS_URL, TABLETS_URL, PHONES_URL, TOUCH_PHONES_URL
+    ]
+    get_all_products(all_links)
